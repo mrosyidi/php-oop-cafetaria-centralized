66569a8b5098d371159a54dd7b62183b0f673cd5
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-15 23:08:26 +0700

    add Food.php

---
+++ Entity/Food.php
@@ -0,0 +1,36 @@
+<?php 
+
+    namespace Entity 
+    {
+        class Food 
+        {
+            private ?string $name;
+            private ?int $price;
+
+            public function __construct(?string $name = null, ?int $price = null)
+            {
+                $this->name = $name;
+                $this->price = $price;
+            }
+
+            public function getName(): ?string 
+            {
+                return $this->name;
+            }
+
+            public function setName(?string $name): void 
+            {
+                $this->name = $name;
+            }
+
+            public function getPrice(): ?int 
+            {
+                return $this->price;
+            }
+
+            public function setPrice(?int $price): void 
+            {
+                $this->price = $price;
+            }
+        }
+    }
