c372a25bb67641868d99cd62668dfe1bccda1e50
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 06:29:14 +0700

    add FoodView.php

---
+++ View/FoodView.php
@@ -0,0 +1,22 @@
+<?php 
+
+    namespace View 
+    {
+        class FoodView 
+        {
+            function showFood(): void 
+            {
+
+            }
+
+            function addFood(): void 
+            {
+
+            }
+
+            function removeFood(): void 
+            {
+                
+            }
+        }
+    }
