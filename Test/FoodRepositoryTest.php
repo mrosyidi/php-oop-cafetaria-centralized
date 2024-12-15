cc810b4a7a72016789820ccc814e7189322d321b
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 06:56:22 +0700

    add FoodRepositoryTest.php

---
+++ Test/FoodRepositoryTest.php
@@ -0,0 +1,20 @@
+<?php 
+
+    require_once __DIR__ . "/../Entity/Food.php";
+    require_once __DIR__ . "/../Repository/FoodRepository.php";
+
+    use Entity\Food;
+    use Repository\FoodRepositoryImpl;
+
+    function testFindAll(): void
+    {
+        $foodRepository = new FoodRepositoryImpl();
+        $food = new Food("Mie Goreng", 6000);
+        $foodRepository->foods[1] = $food;
+        $food = new Food("Soto Ayam", 12000);
+        $foodRepository->foods[2] = $food;
+        $foods = $foodRepository->findAll();
+        var_dump($foods);
+    }
+
+    testFindAll();
