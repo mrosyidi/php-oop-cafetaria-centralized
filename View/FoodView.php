58000ec4d95e1c221d58785961fe5215c43ba369
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 14:10:41 +0700

    update FoodView.php menambah makanan

--- View/FoodView.php
+++ View/FoodView.php
@@ -4,6 +4,7 @@
     {
         use Service\FoodService;
         use Helper\InputHelper;
+        use Helper\CheckHelper;
 
         class FoodView 
         {
@@ -42,7 +43,36 @@
 
             function addFood(): void 
             {
+                $foods = $this->foodService->getFood();
 
+                echo "MENAMBAH MAKANAN" . PHP_EOL;
+
+                $name = InputHelper::input("Nama makanan (x untuk batal)");
+
+                if($name == "x")
+                {
+                    echo "Batal menambah makanan" . PHP_EOL;
+                }else
+                {
+                    $price = InputHelper::input("Harga makanan (x untuk batal)");
+
+                    if($price == "x")
+                    {
+                        echo "Batal menambah makanan" . PHP_EOL;
+                    }else if(!is_numeric($price))
+                    {
+                        echo "Gagal menambah makanan, harga makanan harus bilangan" . PHP_EOL;
+                    }else if($price <= 0)
+                    {
+                        echo "Gagal menambah makanan, harga harus bilangan positif" . PHP_EOL;
+                    }else if(CheckHelper::check($foods, $name))
+                    {
+                        echo "Gagal menambah makanan, nama makanan sudah ada" . PHP_EOL;
+                    }else
+                    {
+                        $this->foodService->addFood($name, $price);
+                    }
+                }
             }
 
             function removeFood(): void 
