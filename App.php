b695f6ed65bd68e22640c8bb505e8649f0922d6b
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 22:31:36 +0700

    update App.php menampilkan menu makanan

--- App.php
+++ App.php
@@ -4,9 +4,54 @@
     require_once __DIR__ . "/Repository/FoodRepository.php";
     require_once __DIR__ . "/Service/FoodService.php";
     require_once __DIR__ . "/View/FoodView.php";
+    require_once __DIR__ . "/Helper/InputHelper.php";
+    require_once __DIR__ . "/Helper/CheckHelper.php";
 
     use Repository\FoodRepositoryImpl;
     use Service\FoodServiceImpl;
     use View\FoodView;
+    use Helper\InputHelper;
+
+    $foodRepository = new FoodRepositoryImpl();
+    $foodService = new FoodServiceImpl($foodRepository);
+    $foodView = new FoodView($foodService);
 
     echo "Cafetaria App" . PHP_EOL;
+
+    while(true)
+    {
+        echo "MENU UTAMA" . PHP_EOL;
+        echo "1. Daftar Makanan" . PHP_EOL;
+        echo "2. Daftar Minuman" . PHP_EOL;
+        echo "3. Pemesanan" . PHP_EOL;
+        echo "4. Pembayaran" . PHP_EOL;
+        echo "5. Detail" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = InputHelper::input("Pilih");
+
+        if($pilihan == "1")
+        {
+            $foodView->showFood();
+        }else if($pilihan == "2")
+        {
+
+        }else if($pilihan == "3")
+        {
+
+        }else if($pilihan == "4")
+        {
+
+        }else if($pilihan == "5")
+        {
+
+        }else if($pilihan == "x")
+        {
+            break;
+        }else 
+        {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
