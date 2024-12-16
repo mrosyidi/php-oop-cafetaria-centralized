fa4128804a73d670107fbacbccef62be6fed7cae
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 21:51:44 +0700

    update FoodRepository.php menghapus makanan

--- Repository/FoodRepository.php
+++ Repository/FoodRepository.php
@@ -28,7 +28,26 @@
 
             public function remove(int $number): bool 
             {
+                $elements = sizeof($this->foods);
 
+                if($number <= 0)
+                {
+                    return false;
+                }
+
+                if($elements < $number)
+                {
+                    return false;
+                }
+
+                for($index = $number; $index < sizeof($this->foods); $index++)
+                {
+                    $this->foods[$index]->setName($this->foods[$index+1]->getName());
+                    $this->foods[$index]->setPrice($this->foods[$index+1]->getPrice());
+                }
+
+                unset($this->foods[sizeof($this->foods)]);
+                return true;
             }
         }
     }
