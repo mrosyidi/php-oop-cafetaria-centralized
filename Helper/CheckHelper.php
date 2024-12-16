df54c9dcd7737755b663323ba26490deba7beb69
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 13:51:26 +0700

    add CheckHelper.php

---
+++ Helper/CheckHelper.php
@@ -0,0 +1,25 @@
+<?php 
+
+    namespace Helper
+    {
+        class CheckHelper
+        {
+            public static function check(array $items, string $name): bool
+            {
+                $found = false;
+
+                foreach($items as $key => $item)
+                {
+                    $item_name = preg_replace('/\s+/', '', strtolower($item->getName()));
+                    $name = preg_replace('/\s+/', '', strtolower($name));
+
+                    if($item_name === $name)
+                    {
+                        $found = true;
+                    }
+                }
+
+                return $found;
+            }
+        }
+    }
