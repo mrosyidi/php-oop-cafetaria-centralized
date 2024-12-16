a25950285df478316b45e39cca40a92135eda8e3
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 10:25:13 +0700

    update FoodViewTest.php mengubah instruksi pada method testViewShowFood

--- Test/FoodViewTest.php
+++ Test/FoodViewTest.php
@@ -1,14 +1,15 @@
 <?php
 
+    require_once __DIR__ . "/../Entity/Food.php";
     require_once __DIR__ . "/../Repository/FoodRepository.php";
     require_once __DIR__ . "/../Service/FoodService.php";
     require_once __DIR__ . "/../View/FoodView.php";
     require_once __DIR__ . "/../Helper/InputHelper.php";
 
+    use Entity\Food;
     use Repository\FoodRepositoryImpl;
     use Service\FoodServiceImpl;
     use View\FoodView;
-
 
     function testViewShowFoodEmpty(): void
     {
@@ -18,4 +19,14 @@
         $foodView->showFood();
     }
 
-    testViewShowFoodEmpty();
+    function testViewShowFoodNotEmpty(): void
+    {
+        $foodRepository = new FoodRepositoryImpl();
+        $foodRepository->save(new Food("Ayam Panggang", 15000));
+        $foodRepository->save(new Food("Pastel", 5000));
+        $foodService = new FoodServiceImpl($foodRepository);
+        $foodView = new FoodView($foodService);
+        $foodView->showFood();
+    }
+
+    testViewShowFoodNotEmpty();
