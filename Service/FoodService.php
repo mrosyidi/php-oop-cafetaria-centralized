e5dde829b5d9244774033f410bfefafe6cab10e4
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-16 07:36:50 +0700

    update FoodService.php menampilkan makanan

--- Service/FoodService.php
+++ Service/FoodService.php
@@ -2,11 +2,56 @@
 
     namespace Service 
     {
+        use Repository\FoodRepository;
+
         interface FoodService 
         {
             public function showFood(): void;
             public function addFood(string $name, int $price): void;
             public function getFood(): array;
             public function removeFood(int $number): bool;
+        }
+
+        class FoodServiceImpl implements FoodService 
+        {
+            private FoodRepository $foodRepository;
+
+            public function __construct(FoodRepository $foodRepository)
+            {
+                $this->foodRepository = $foodRepository;
+            }
+
+            public function showFood(): void 
+            {
+                echo "DAFTAR MAKANAN" . PHP_EOL;
+
+                $foods = $this->foodRepository->findAll();
+
+                if(empty($foods))
+                {
+                    echo "Tidak ada daftar makanan" . PHP_EOL;
+                }else
+                {
+                    foreach($foods as $number => $food)
+                    {
+                        echo "$number. " . $food->getName() . "  Rp." . $food->getPrice() . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addFood(string $name, int $price): void 
+            {
+
+            }
+
+            public function getFood(): array 
+            {
+
+            }
+
+            public function removeFood(int $number): bool 
+            {
+
+            }
         }
     }
