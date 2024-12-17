3f3f8ad385a10a101be12e331e1ea3c12a8feaa8
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-17 11:58:19 +0700

    update DrinkView.php menambah minuman

--- View/DrinkView.php
+++ View/DrinkView.php
@@ -4,6 +4,7 @@
     {
         use Service\DrinkService;
         use Helper\InputHelper;
+        use Helper\CheckHelper;
 
         class DrinkView
         {
@@ -45,7 +46,36 @@
 
             public function addDrink(): void 
             {
+                $drinks = $this->drinkService->getDrink();
 
+                echo "MENAMBAH MINUMAN" . PHP_EOL;
+
+                $name = InputHelper::input("Nama minuman (x untuk batal)");
+
+                if($name == "x")
+                {
+                    echo "Batal menambah minuman" . PHP_EOL;
+                }else
+                {
+                    $price = InputHelper::input("Harga minuman (x untuk batal)");
+
+                    if($price == "x")
+                    {
+                        echo "Batal menambah minuman" . PHP_EOL;
+                    }else if(!is_numeric($price))
+                    {
+                        echo "Gagal menambah minuman, harga minuman harus bilangan" . PHP_EOL;
+                    }else if($price <= 0)
+                    {
+                        echo "Gagal menambah minuman, harga harus bilangan positif" . PHP_EOL;
+                    }else if(CheckHelper::check($drinks, $name))
+                    {
+                        echo "Gagal menambah minuman, nama minuman sudah ada" . PHP_EOL;
+                    }else
+                    {
+                        $this->drinkService->addDrink($name, $price);
+                    }
+                }
             }
 
             public function removeDrink(): void 
