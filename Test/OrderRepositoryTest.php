2b3520836e93f7114b77df299abd063bc3519098
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-17 18:34:18 +0700

    update OrderRepositoryTest.php menampilkan pemesanan

--- Test/OrderRepositoryTest.php
+++ Test/OrderRepositoryTest.php
@@ -17,4 +17,17 @@
         var_dump($orders);
     }
 
-    testFindAllEmpty();
+    function testFindAllNotEmpty(): void
+    {
+        $food = new Food("Mie Goreng", 6000);
+        $orderRepository = new OrderRepositoryImpl();
+        $orderRepository->orders[1] = new Order(1, $food->getName(), $food->getPrice(), 1);
+        $drink = new Drink("Es Campur", 12000);
+        $orderRepository->orders[2] = new Order(1, $drink->getName(), $drink->getPrice(), 2);
+        $food = new Food("Pastel", 5000);
+        $orderRepository->orders[3] = new Order(1, $food->getName(), $food->getPrice(), 3);
+        $orders = $orderRepository->findAll();
+        var_dump($orders);
+    }
+
+    testFindAllNotEmpty();
