de850ea06aa0ccd396a615d544d59d74b7aeeffd
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-17 07:57:03 +0700

    add DrinkRepositoryTest.php

---
+++ Test/DrinkRepositoryTest.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+    require_once __DIR__ . "/../Entity/Drink.php";
+    require_once __DIR__ . "/../Repository/DrinkRepository.php";
+
+    use Entity\Drink;
+    use Repository\DrinkRepositoryImpl;
+
+    function testFindAllEmpty(): void
+    {
+        $drinkRepository = new DrinkRepositoryImpl();
+        $drinks = $drinkRepository->findAll();
+        var_dump($drinks);
+    }
+
+    function testFindAllNotEmpty(): void
+    {
+        $drinkRepository = new DrinkRepositoryImpl();
+        $drinkRepository->drinks[1] = new Drink("Es Teh", 3000);
+        $drinkRepository->drinks[2] = new Drink("Es Coklat", 12000);
+        $drinkRepository->drinks[3] = new Drink("Jus Wortel", 8000);
+        $drinks = $drinkRepository->findAll();
+        var_dump($drinks);
+    }
+
+    testFindAllNotEmpty();
