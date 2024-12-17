3b512494a1b9ede05d27ed719512489092b83894
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-17 13:24:03 +0700

    update DrinkView.php menghapus minuman

--- View/DrinkView.php
+++ View/DrinkView.php
@@ -80,7 +80,28 @@
 
             public function removeDrink(): void 
             {
+                echo "MENGHAPUS MINUMAN" . PHP_EOL;
 
+                $number = InputHelper::input("Nomor minuman (x untuk batal)");
+
+                if($number == "x")
+                {
+                    echo "Batal menghapus minuman" . PHP_EOL;
+                }else if(!is_numeric($number))
+                {
+                    echo "Gagal menghapus minuman, nomor harus bilangan" . PHP_EOL;
+                }else
+                {
+                    $success = $this->drinkService->removeDrink($number);
+
+                    if($success)
+                    {
+                        echo "Sukses menghapus minuman nomor $number" . PHP_EOL;
+                    }else
+                    {
+                        echo "Gagal menghapus minuman nomor $number" . PHP_EOL;
+                    }
+                }
             }
         }
     }
