5ba12cba4766b26a1cd24a4a5cb703e3eb690d26
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-18 23:20:37 +0700

    update OrderSeriveTest.php menampilkan pemesanan

--- Service/OrderService.php
+++ Service/OrderService.php
@@ -11,4 +11,43 @@
             public function addOrder(int $code, string $name, int $price, int $qty): void;
             public function getOrder(): array;
         }
+
+        class OrderServiceImpl implements OrderService
+        {
+            private OrderRepository $orderRepository;
+
+            public function __construct(OrderRepository $orderRepository)
+            {
+                $this->orderRepository = $orderRepository;
+            }
+
+            public function showOrder(): void
+            {
+                $orders = $this->orderRepository->findAll();
+
+                echo "DAFTAR PESANAN" . PHP_EOL;
+
+                if(empty($orders))
+                {
+                    echo "Tidak ada daftar pesanan" . PHP_EOL;
+                }else
+                {
+                    foreach($orders as $number => $order)
+                    {
+                        echo "$number. " . $order->getCode() . " " . $order->getName() . " Rp." . $order->getPrice() .
+                        " (x" . $order->getQty() . ") Rp." . $order->getSubTotal() . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addOrder(int $code, string $name, int $price, int $qty): void 
+            {
+
+            }
+
+            public function getOrder(): array
+            {
+                
+            }
+        }
     }
