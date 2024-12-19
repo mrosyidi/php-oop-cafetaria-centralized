0e854a4927e869056f598634dd8748c7be5a3e30
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-19 18:17:23 +0700

    update OrderView.php menampilkan daftar pemesanan

--- View/OrderView.php
+++ View/OrderView.php
@@ -19,5 +19,39 @@
                 $this->foodService = $foodService;
                 $this->drinkService = $drinkService;
             }
+
+            public function showOrder(): void 
+            {
+                while(true)
+                {
+                    $this->orderService->showOrder();
+
+                    echo "Menu Pemesanan" . PHP_EOL;
+                    echo "1. Pesan Makanan" . PHP_EOL;
+                    echo "2. Pesan Minuman" . PHP_EOL;
+                    echo "x. Kembali" . PHP_EOL;
+
+                    $pilihan = InputHelper::input("Pilih");
+
+                    if($pilihan == "1")
+                    {
+
+                    }else if($pilihan == "2")
+                    {
+                        
+                    }else if($pilihan == "x")
+                    {
+                        break;
+                    }else
+                    {
+                        echo "Pilihan tidak dimengerti" . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addOrder(int $numberOrder, bool $exit): void 
+            {
+
+            }
         }
     }
