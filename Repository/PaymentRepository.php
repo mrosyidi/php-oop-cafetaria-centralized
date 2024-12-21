21ea51e3ec4a5f7bd7cbb3c8dae006a211d7e768
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-21 11:33:24 +0700

    update PaymentRepository.php menampilkan daftar pembayaran

--- Repository/PaymentRepository.php
+++ Repository/PaymentRepository.php
@@ -9,4 +9,19 @@
             public function findAll(): array;
             public function save(Payment $payment): void;
         }
+
+        class PaymentRepositoryImpl implements PaymentRepository
+        {
+            public array $payments = array();
+
+            public function findAll(): array
+            {
+                return $this->payments;
+            }
+
+            public function save(Payment $payment): void 
+            {
+                
+            } 
+        }
     }
