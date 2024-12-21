7e5aaa94f38cc3d45e6d39a142c18c6c7c913453
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-21 16:28:39 +0700

    update PaymentServiceTest.php menampilkanf daftar pembayaran ada data yang ditampilkan

--- Test/PaymentServiceTest.php
+++ Test/PaymentServiceTest.php
@@ -25,4 +25,22 @@
         $paymentService->showPayment();
     }
 
-    testShowPaymentEmpty();
+    function testShowPaymentNotEmpty(): void
+    {
+        $orderRepository = new OrderRepositoryImpl();
+        $paymentRepository = new PaymentRepositoryImpl();
+        $paymentService = new PaymentServiceImpl($paymentRepository);
+        $food = new Food("Mie Goreng", 6000);
+        $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 1));
+        $food = new Food("Soto Ayam", 12000);
+        $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 1));
+        $drink = new Drink("Es Campur", 12000);
+        $orderRepository->save(new Order(2, $drink->getName(), $drink->getPrice(), 2));
+        $orders = $orderRepository->findAll();
+        $orders = array_filter($orders, fn($order)=>$order->getCode() == 1);
+        $total = array_sum(array_map(fn($order)=>$order->getSubTotal(), $orders));
+        $paymentRepository->payments[1] = new Payment(1, $total, 50000);
+        $paymentService->showPayment();
+    }
+
+    testShowPaymentNotEmpty();
