3220ba0509e01d66c0e60c0ab4168af0187e9f33
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-21 16:25:02 +0700

    update PaymentService.php menampilkanf daftar pembayaran

--- Service/PaymentService.php
+++ Service/PaymentService.php
@@ -11,4 +11,45 @@
             public function addPayment(int $code, int $total, int $pay): void;
             public function getPayment(): array;
         }
+
+        class PaymentServiceImpl implements PaymentService
+        {
+            private PaymentRepository $paymentRepository;
+
+            public function __construct(PaymentRepository $paymentRepository)
+            {
+                $this->paymentRepository = $paymentRepository;
+            }
+
+            public function showPayment(): void
+            {
+                echo "DAFTAR PEMBAYARAN" . PHP_EOL;
+
+                $payments = $this->paymentRepository->findAll();
+
+                if(empty($payments))
+                {
+                    echo "Tidak ada daftar pembayaran" . PHP_EOL;
+                }else
+                {
+                    $counter = 0;
+                    foreach($payments as $payment)
+                    {
+                        $counter++;
+                        echo "$counter. Kode: " . $payment->getCode() . "  Total: " . $payment->getTotal() .
+                        "  Jumlah Bayar: " . $payment->getPay() . "  Kembalian: " . $payment->getChange() . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addPayment(int $code, int $total, int $pay): void
+            {
+
+            }
+
+            public function getPayment(): array
+            {
+                
+            }
+        }
     }
