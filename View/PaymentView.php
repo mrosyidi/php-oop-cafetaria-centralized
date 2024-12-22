304a53ce706835aa68d2d58c4d282f7412dde102
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-22 12:48:29 +0700

    update PaymentView.php menambah data pembayaran

--- View/PaymentView.php
+++ View/PaymentView.php
@@ -4,7 +4,9 @@
     {
         use Service\OrderService;
         use Service\PaymentService;
+        use Helper\FindHelper;
         use Helper\InputHelper;
+        use Helper\PayHelper;
     
         class PaymentView
         {
@@ -38,6 +40,50 @@
                     }else
                     {
                         echo "Pilihan tidak dimengerti" . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addPayment(): void
+            {
+                echo "PEMBAYARAN PESANAN" . PHP_EOL;
+                $code = InputHelper::input("Kode Pesanan (x untuk batal)");
+
+                if($code == "x")
+                {
+                    echo "Batal memproses pesanan" . PHP_EOL;
+                }else
+                {
+                    $orders = $this->orderService->getOrder();
+                    $codeOrder = FindHelper::find($orders, $code);
+
+                    if($codeOrder)
+                    {
+                        $pay = PayHelper::pay($orders, $code);
+                        echo "Total yang harus dibayar : Rp." . $pay . PHP_EOL;
+
+                        $money = InputHelper::input("Jumlah uang (x untuk batal)");
+
+                        if($money == "x")
+                        {
+                            echo "Batal memproses pesanan" . PHP_EOL;
+                        }else if(!is_numeric($money))
+                        {
+                            echo "Gagal memproses pesanan, jumlah uang harus bilangan" . PHP_EOL;
+                        }else if($money < $pay)
+                        {
+                            echo "Gagal memproses pesanan, jumlah uang yang digunakan tidak cukup" . PHP_EOL;
+                        }else
+                        {
+                            $change = $money-$pay;
+                            $this->paymentService->addPayment($code, $pay, $money);
+
+                            echo "Kembalian : Rp." . $change . PHP_EOL;
+                            echo "Sukses membayar pesanan" . PHP_EOL;
+                        }
+                    }else
+                    {
+                        echo "Kode pesanan tidak ditemukan" . PHP_EOL;
                     }
                 }
             }
