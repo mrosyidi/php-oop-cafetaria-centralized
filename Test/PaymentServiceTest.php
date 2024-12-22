b0658b0c5a4646e7f321617763cf7130672d16ef
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-22 12:13:55 +0700

    update PaymentServiceTest.php menambah data pembayaran

--- Test/PaymentServiceTest.php
+++ Test/PaymentServiceTest.php
@@ -43,4 +43,22 @@
         $paymentService->showPayment();
     }
 
-    testShowPaymentNotEmpty();
+    function testAddPayment(): void
+    {
+        $orderRepository = new OrderRepositoryImpl();
+        $paymentRepository = new PaymentRepositoryImpl();
+        $paymentService = new PaymentServiceImpl($paymentRepository);
+        $food = new Food("Mie Goreng", 6000);
+        $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 1));
+        $food = new Food("Soto Ayam", 12000);
+        $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 1));
+        $drink = new Drink("Es Campur", 12000);
+        $orderRepository->save(new Order(1, $drink->getName(), $drink->getPrice(), 2));
+        $orders = $orderRepository->findAll();
+        $orders = array_filter($orders, fn($order)=>$order->getCode() == 1);
+        $total = array_sum(array_map(fn($order)=>$order->getSubTotal(), $orders));
+        $paymentService->addPayment(1, $total, 50000);
+        $paymentService->showPayment();
+    }
+
+    testAddPayment();
