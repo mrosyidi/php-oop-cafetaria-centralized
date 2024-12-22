f59482fcef5711a807f09887f00134f6a8df44ed
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-22 12:08:48 +0700

    update PaymentRepositoryTest.php mengubah intruksi method testFindAllNotEmpty

--- Test/PaymentRepositoryTest.php
+++ Test/PaymentRepositoryTest.php
@@ -34,7 +34,7 @@
         $orders = $orderRepository->findAll();
         $orders = array_filter($orders, fn($order)=>$order->getCode() == 1);
         $total = array_sum(array_map(fn($order)=>$order->getSubTotal(), $orders));
-        $paymentRepository->payments[1] = new Payment(1, $total, 50000);
+        $paymentRepository->save(new Payment(1, $total, 50000));
         $payments = $paymentRepository->findAll();
         var_dump($payments);
     }
@@ -57,4 +57,4 @@
         var_dump($payments);
     }
 
-    testSave();
+    testFindAllNotEmpty();
