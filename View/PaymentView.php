203417ea77cc4d11af30d2014d789cb4a974ef96
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-22 11:59:29 +0700

    update PaymentView.php menampilkan daftar pemesanan

--- View/PaymentView.php
+++ View/PaymentView.php
@@ -16,5 +16,30 @@
                 $this->paymentService = $paymentService;
                 $this->orderService = $orderService;
             }
+
+            public function showPayment(): void
+            {
+                while(true)
+                {
+                    $this->orderService->showOrder();
+
+                    echo "Menu Pembayaran" . PHP_EOL;
+                    echo "1. Bayar Pesanan" . PHP_EOL;
+                    echo "x. Kembali" . PHP_EOL;
+
+                    $pilihan = InputHelper::input("Pilih");
+
+                    if($pilihan == "1")
+                    {
+                        
+                    }else if($pilihan == "x")
+                    {
+                        break;
+                    }else
+                    {
+                        echo "Pilihan tidak dimengerti" . PHP_EOL;
+                    }
+                }
+            }
         }
     }
