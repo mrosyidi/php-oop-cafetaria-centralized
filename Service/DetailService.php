6b506e82a4a2ec063760d1e8df5680fb3b3c1ce3
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-23 22:33:44 +0700

    update DetailService.php menampilkan detail pembayaran

--- Service/DetailService.php
+++ Service/DetailService.php
@@ -10,4 +10,43 @@
             public function showDetail(int $code): void;
             public function addDetail(array $items): void;
         }
+
+        class DetailServiceImpl implements DetailService
+        {
+            private DetailRepository $detailRepository;
+
+            public function __construct(DetailRepository $detailRepository)
+            {
+                $this->detailRepository = $detailRepository;
+            }
+
+            public function showDetail(int $code): void
+            {
+                $details = $this->detailRepository->findAll();
+
+                echo "DAFTAR DETAIL" . PHP_EOL;
+
+                if(empty($details))
+                {
+                    echo "Tidak ada daftar detail" . PHP_EOL;
+                }else
+                {
+                    $counter = 0;
+                    foreach($details as $detail)
+                    {
+                        if($detail->getCode() == $code)
+                        {
+                            $counter++;
+                            echo "$counter. " . $detail->getCode() . " " . $detail->getName() . " Rp." . $detail->getPrice() .
+                            " (x" . $detail->getQty() . ") Rp." . $detail->getSubTotal() . " " .  $detail->getStatus() . PHP_EOL;
+                        }
+                    }
+                }
+            }
+
+            public function addDetail(array $items): void
+            {
+                
+            }
+        }
     }
