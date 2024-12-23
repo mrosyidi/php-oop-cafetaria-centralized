ae42c08ba085798d9f286d47f6ded5964402e7e0
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-23 08:58:25 +0700

    add update DetailRepository.php menampilkan detail pemesanan

--- Repository/DetailRepository.php
+++ Repository/DetailRepository.php
@@ -9,4 +9,19 @@
             public function findAll(): array;
             public function save(Detail $detail): void;
         }
+
+        class DetailRepositoryImpl implements DetailRepository
+        {
+            public array $details = array();
+
+            public function findAll(): array
+            {
+                return $this->details;
+            }
+
+            public function save(Detail $detail): void
+            {
+
+            }
+        }
     }
