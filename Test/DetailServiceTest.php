b653ca140a7754a13f7fb80886f9668a28cd5d8d
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-25 13:58:57 +0700

    update DetailServiceTest.php mengubah intruksi method testShowDetailNotEmpty

--- Test/DetailServiceTest.php
+++ Test/DetailServiceTest.php
@@ -39,13 +39,13 @@
         $paymentService = new PaymentServiceImpl($paymentRepository);
         $food = new Food("Mie Goreng", 6000);
         $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 1));
-        $detailRepository->details[1] = new Detail(1, $food->getName(), $food->getPrice(), 1);
+        $detailRepository->save(new Detail(1, $food->getName(), $food->getPrice(), 1));
         $food = new Food("Soto Ayam", 12000);
         $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 1));
-        $detailRepository->details[2] = new Detail(1, $food->getName(), $food->getPrice(), 1);
+        $detailRepository->save(new Detail(1, $food->getName(), $food->getPrice(), 1));
         $drink = new Drink("Es Campur", 12000);
         $orderRepository->save(new Order(2, $drink->getName(), $drink->getPrice(), 2));
-        $orderRepository->details[3] = new Detail(2, $drink->getName(), $drink->getPrice(), 2);
+        $detailRepository->save(new Detail(2, $drink->getName(), $drink->getPrice(), 2));
         $orders = $orderRepository->findAll();
         $orders = array_filter($orders, fn($order)=>$order->getCode() == 1);
         $total = array_sum(array_map(fn($order)=>$order->getSubTotal(), $orders));
@@ -81,4 +81,4 @@
         var_dump($details);
     }
 
-    testAddDetail();
+    testShowDetailNotEmpty();
