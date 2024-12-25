79a43ca7b5c6df33f5fd465cbfe05df546ae2520
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-25 16:21:00 +0700

    update CodeHelper.php generate kode pemesanan berdasarkan pembayaran

--- Helper/CodeHelper.php
+++ Helper/CodeHelper.php
@@ -4,17 +4,30 @@
     {
         class CodeHelper
         {
-            public static function code(array $orders, bool $exit): int
+            public static function code(array $orders, array $payments, bool $exit): int
             {
-                if(empty($orders))
+                if(empty($orders) && empty($payments))
                 {
                     $code = 1;
-                }else if(!empty($orders) && !$exit)
+                }else if(empty($orders) && !empty($payments))
+                {                    
+                    $max = max(array_column($payments, 'code'));
+                    $code = $max + 1;
+                }
+                else if(!empty($orders) && !$exit)
                 {
                     $code = $orders[sizeof($orders)]->getCode();
                 }else if(!empty($orders) && $exit)
                 {
-                    $code = $orders[sizeof($orders)]->getCode() + 1;
+                    $max = max(array_column($payments, 'code'));
+
+                    if(!empty($payments))
+                    {
+                        $paymentMax = max(array_column($payments, 'code'));
+                        $max = $max < $paymentMax ? $paymentMax : $max;
+                    }
+
+                    $code = $max + 1;
                 }
 
                 return $code;
