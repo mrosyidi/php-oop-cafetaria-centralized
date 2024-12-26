dffae84007e8e7cddb0141eed23c5ce808fbdbfb
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-26 12:19:55 +0700

    update DuplicateHelperTest.php ada data yang digandakan

--- Test/DuplicateHelperTest.php
+++ Test/DuplicateHelperTest.php
@@ -21,5 +21,24 @@
         $elements = DuplicateHelper::duplicate($orders, 2);
         var_dump($elements);
     }
-    
-    testDuplicateEmpty();
+
+    function testDuplicateNotEmpty(): void 
+    {
+        $food = new Food("Mie Goreng", 6000);
+        $orderRepository = new OrderRepositoryImpl();
+        $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 1));
+        $drink = new Drink("Es Campur", 12000);
+        $orderRepository->save(new Order(1, $drink->getName(), $drink->getPrice(), 2));
+        $food = new Food("Pastel", 5000);
+        $orderRepository->save(new Order(1, $food->getName(), $food->getPrice(), 3));
+        $food = new Food("Soto Ayam", 10000);
+        $orderRepository->save(new Order(2, $food->getName(), $food->getPrice(), 2));
+        $drink = new Drink("Es Oyen", 12000);
+        $orderRepository->save(new Order(2, $drink->getName(), $drink->getPrice(), 2));
+        $orders = $orderRepository->findAll();
+        $elements = DuplicateHelper::duplicate($orders, 2);
+        var_dump($elements);
+    }
+
+
+    testDuplicateNotEmpty();
